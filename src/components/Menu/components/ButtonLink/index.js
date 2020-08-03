eeee46d30785271ebf066958af5bf39bc2b6397b
Gustavo Malveira <gustavoagl@github.com>
2020-08-03 12:35:48 -0300

    Setup inicial do Projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function ButtonLink(props) {
+    return (
+        <a href={rops.href} className={props.className}>
+            {props.children}
+         </a>
+    );
+}
+
+export default ButtonLink;
