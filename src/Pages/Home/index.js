aa356b7d0a41688dd577791f20a00a4919f02ea1
Gustavo Malveira <gustavoagl@github.com>
2020-08-05 17:38:36 -0300

    Melhorando

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 
-function App() {
+function Home() {
 
   return (
 
@@ -22,7 +22,7 @@ function App() {
 
         url={dadosIniciais.categorias[0].videos[0].url}
 
-        videoDescription={"O que é Front-end"}
+        videoDescription={""}
 
       />
 
@@ -90,4 +90,4 @@ function App() {
 
 
 
-export default App;
+export default Home;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button'
+import { Link } from 'react-router-dom';
+
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
         <img className="Logo" src={Logo} alt="HaterFlix" /> 
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={ Link } className="ButtonLink" to="/cadastro/video">
                 Novo Video! 
                 </Button>
          </nav>
